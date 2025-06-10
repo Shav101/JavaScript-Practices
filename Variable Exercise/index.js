e5f25acd95f4b31645f3f29a6736f6bb9cc054aa
Shav101 <shavkat192710@gmail.com>
2025-06-10 00:58:43 -0400

    Added Variable Exercise

---
+++ Variable Exercise/index.js
@@ -0,0 +1,47 @@
+/*
+Exercise
+
+Consider a car.  What attributes and properties of a car could we capture using variables?
+What type (i.e. string, number, boolean) would you use for each one?
+
+Create variables for each of the following attributes of a car and assign it a value.
+
+Example: var hasPowerWindows = true;
+
+1.  make
+2.  model
+3.  year
+4.  color
+5.  isManualTransmission
+6.  numSeats
+7.  mileage
+*/
+
+var make = "Toyota";
+var model = "Sienna";
+var year = 2025;
+var color = "White";
+var isManualTransmission = false;
+var numSeats = 7;
+var mileage = 5000;
+
+console.log("Value: ", make);
+console.log("Type: ", typeof make);
+
+console.log("Value: ", model);
+console.log("Type: ", typeof model);
+
+console.log("Value: ", year);
+console.log("Type: ", typeof year);
+
+console.log("Value: ", color);
+console.log("Type: ", typeof color);
+
+console.log("Value: ", isManualTransmission);
+console.log("Type: ", typeof isManualTransmission);
+
+console.log("Value: ", numSeats);
+console.log("Type: ", typeof numSeats);
+
+console.log("Value: ", mileage);
+console.log("Type: ", typeof mileage);
